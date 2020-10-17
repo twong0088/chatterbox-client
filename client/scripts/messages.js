64f63e8e0e06ee39e6db6044cb8f1e2c2f998944
Marketgai <marcusalee@gmail.com>
2020-10-16 22:08:17 -0700

    added messages.js

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,20 +1,12 @@
 var Messages = {
   format: function (arr) {
-    for (var i = 0; i < arr.length; i++) {
 
+    for (var i = 0; i < arr.length; i++) {
+      MessagesView.renderMessage(arr[i]);
       // if (!arr[i].username) {
       //   arr[i].username = 'anonymous';
       // }
-      arr[i].username = arr[i].username || 'anonymous';
-      // if (!arr[i].roomname) {
-      //   arr[i].roomname = 'main';
-      // }
-      arr[i].roomname = arr[i].roomname || 'main';
 
-      if (arr[i].text) {
-        var message = MessageView.render(arr[i]);
-        MessagesView.$chats.append(message);
-      }
     }
   }
 };
